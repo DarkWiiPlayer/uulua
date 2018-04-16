d210d133caa25d2d3fc978a6b25906f370bc09e3
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2018-04-16 14:57:23 +0200

    Fix lua code after porting from moonscript
    Next time I port somethin I will test it before commiting ;)

--- src/main.lua
+++ src/main.lua
@@ -1,7 +1,8 @@
 math.randomseed(os.time())
-math.random()
-math.random()
-math.random()
+local random = math.random
+random()
+random()
+random()
 
 local M = {}
 
@@ -9,19 +10,21 @@ local M = {}
 -- Numbers larger than 15 probably return nil,
 -- but this behavior is not garanteed
 local function tohex(num)
-  string.char("0123456789abcdef"):byte(num+1)
+  return string.char(("0123456789abcdef"):byte(num+1))
 end
 
 --- Generates a version 4 type 1 UUID
 -- does not accept any arguments
 function M.v4_1()
-  local uuid = ("xxxxxxxx-xxxx-axxx-bxxx-xxxxxxxxxxxx"):gsub("a", function(a)
+  local uuid = "xxxxxxxx-xxxx-axxx-bxxx-xxxxxxxxxxxx"
+  uuid = uuid:gsub("a", function(a)
     return "4"
   end):gsub("b", function(b)
     return tohex(8+random(3))
   end):gsub("x", function(x)
     return tohex(random(15))
   end)
+  return uuid
 end
 
 return M
